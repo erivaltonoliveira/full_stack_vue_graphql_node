b4cd745904fb07e5c2d4e226bed3521476e892fa
erivaltonoliveira <erivaltonsantos@gmail.com>
2019-10-29 10:37:41 -0300

    ...

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>
+        NameGator
+        <span class="fa fa-check"></span>
+        <br />
+      </h1>
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node.js</h6>
+    </div>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+  name: "app",
+  components: {
+    DomainList
+  }
+};
+</script>
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
